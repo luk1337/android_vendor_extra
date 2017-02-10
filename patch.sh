6bcbafcdc011f649eadb198c3c7c26d378384e24
LuK1337 <priv.luk@gmail.com>
2017-02-10 16:27:22 +0100

    Initial commit

---
+++ patch.sh
@@ -0,0 +1,20 @@
+ROOT='/home/luk/android/lineage-14.1'
+REPOSITORIES=(
+    'packages/apps/ContactsCommon'
+    'packages/apps/Contacts'
+    'packages/apps/Settings'
+    'packages/apps/ExactCalculator'
+    'packages/apps/PhoneCommon'
+    'build'
+    'system/sepolicy'
+    'frameworks/native'
+    'frameworks/base'
+)
+
+for repository in "${REPOSITORIES[@]}"; do
+    cd "${ROOT}/${repository}"
+
+    git am "${ROOT}/oms-patches/${repository}"/*
+
+    cd "${ROOT}"
+done
