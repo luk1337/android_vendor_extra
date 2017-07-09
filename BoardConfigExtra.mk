08d4f5571761a5e5a9ee74927a53ea568ed3c2f0
LuK1337 <priv.luk@gmail.com>
2017-07-09 12:59:39 +0200

    Include vendor/luk

---
+++ BoardConfigExtra.mk
@@ -0,0 +1,1 @@
+-include vendor/luk/BoardConfigExtra.mk
--- product.mk
+++ product.mk
@@ -2,3 +2,5 @@
 PRODUCT_PACKAGES += \
     ThemeInterfacer \
     projekt.substratum.theme.xml
+
+$(call prepend-product-if-exists, vendor/luk/product.mk)
