1c4545beab2be23dacecbfb976a5d6271f0dc41a
Bruno Martins <bgcngm@gmail.com>
2021-01-04 12:19:23 +0000

    Move default adb shell prop to prop.default
    This allows us to get rid of TARGET_SYSTEM_PROP usage
    as it breaks devices with standalone device trees that don't
    set such flag. Note that setting that flag is only needed
    in common device trees.

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -2,6 +2,3 @@
 ifeq ($(WITH_GMS), true)
 include vendor/gapps/common/BoardConfigVendor.mk
 endif
-
-# Properties
-TARGET_SYSTEM_PROP += vendor/extra/system.prop
--- product.mk
+++ product.mk
@@ -1,3 +1,7 @@
+# Default ADB shell prompt
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.sys.adb.shell=/system/xbin/bash
+
 # Google Apps
 ifeq ($(WITH_GMS), true)
 $(call inherit-product, vendor/gapps/common/common-vendor.mk)
